c6eb5e7f04a518c924b052464651c5c8224a7ea1
yanovich <>
2005-06-02 04:40:50 +0000

    initial checkin

---
+++ doc/mk/pdl.mk
@@ -0,0 +1,35 @@
+# $Id$
+
+OBJDIR = obj
+
+.SUFFIXES: .pdl .html
+
+HTML = ${LIBS:S/$/.html/}
+
+.include "local.mk"
+
+ADJSYSROOT = ${.CURDIR}/${SYSROOT}
+
+all: all-recursive docs
+
+docs: doc-html
+
+doc-html: ${HTML}
+
+.pdl.html:
+	${XSLTPROC} -o ${.TARGET} ${ADJSYSROOT}/doc/xsl/doc-html.xsl ${.IMPSRC}
+
+obj: obj-recursive
+	mkdir ${OBJDIR}
+
+clean: clean-recursive
+	rm -f ${HTML}
+
+all-recursive clean-recursive obj-recursive:
+	@for i in ${SUBDIRS}; do					\
+		echo "==> $$i";						\
+		(cd ${.CURDIR}/$$i && make ${.TARGET:S/-recursive//});	\
+	done
+	@if [ -n "${SUBDIRS}" ]; then					\
+		echo "<==";						\
+	fi
